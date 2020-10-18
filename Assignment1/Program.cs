8a401fa2e28dc1bae9f08a7eb33425e56fa43e74
Ahmed Ghareeb <a.s.ghareeb@gmail.com>
2020-10-17 22:15:43 -0400

    first commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			//read user input and
+			//split the input into an array
+			string[] inputArray = getUserInput().Split('+');
+
+			//print the result
+			try
+			{
+				Console.WriteLine("your result is: "+add(inputArray[0], inputArray[1]));
+			}
+			catch {
+				Console.WriteLine("Enter only numbers in format x+y");
+			}
+			
+		}
+
+		static int add(String x , String y)
+        {
+			int num1, num2;
+
+			//convert the string into int
+			num1 = Convert.ToInt32(x);
+			num2 = Convert.ToInt32(y);
+			return num1 + num2;
+        }
+
+		static string getUserInput()
+        {
+			//get user input 
+			Console.WriteLine("Enter your problem in format x+y");
+			return Console.ReadLine();
+		}
+    }
+}
